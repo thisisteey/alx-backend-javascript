6a4638423fdecf36a50d1a27b774e3ff3248fd32
thisisteey <dadataiwo82@yahoo.com>
2024-05-30 16:16:24 +0000

    Feature: Implement ClassRoom class with maxStudentsSize attribute

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,6 @@
+export default class ClassRoom {
+  // ClassRoom class to represent a classroom with the maximum number of students
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}

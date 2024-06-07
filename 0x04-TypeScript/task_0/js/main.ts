dc2faeae65c76cb2f1b520a32c9be58c0c926485
thisisteey <dadataiwo82@yahoo.com>
2024-06-07 22:55:38 +0000

    Feature: task 0 complete

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,45 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const student1: Student = {
+    firstName: 'Tim',
+    lastName: 'Marcus',
+    age: 32,
+    location: 'Lagos',
+}
+
+const student2: Student = {
+    firstName: 'Daniel',
+    lastName: 'James',
+    age: 30,
+    location: 'Lagos',
+}
+
+const studentsList: Array<Student> = [student1, student2];
+
+const body: HTMLBodyElement = document.getElementsByTagName('body')[0];
+const table: HTMLTableElement = document.createElement('table');
+const thead: HTMLTableSectionElement = document.createElement('thead');
+const tbody: HTMLTableSectionElement = document.createElement('tbody');
+const rowHead: HTMLTableRowElement = thead.insertRow(0);
+const cell1Head: HTMLTableCellElement = rowHead.insertCell(0);
+const cell2Head: HTMLTableCellElement = rowHead.insertCell(1);
+
+cell1Head.innerHTML = 'firstName';
+cell2Head.innerHTML = 'location';
+table.append(thead);
+
+studentsList.forEach((student) => {
+    const row: HTMLTableRowElement = tbody.insertRow(0);
+    const cell1: HTMLTableCellElement = row.insertCell(0);
+    const cell2: HTMLTableCellElement = row.insertCell(1);
+    cell1.innerHTML = student.firstName;
+    cell2.innerHTML = student.location;
+});
+
+table.append(tbody);
+body.append(table);

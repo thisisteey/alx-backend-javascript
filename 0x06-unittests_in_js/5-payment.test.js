7222a6c79d37cb03ef961b24b6bb11c58792ff0d
thisisteey <dadataiwo82@yahoo.com>
2024-07-26 02:46:38 +0000

    Feature: Added hooks for setup and teardown in test suite for sendPaymentRequestToApi

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,27 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleSpy;
+
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('log "The total is: 10" when called wth 100 and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
+    expect(consoleSpy.calledOnce).to.be.true;
+  });
+
+  it('log "The total is: 20" when called with 10 and 10', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(consoleSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
+    expect(consoleSpy.calledOnce).to.be.true;
+  });
+});
